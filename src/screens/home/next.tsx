79a96ddd6c7b8d3e16c25194275c0abae58dfaae
nslm <56803550+nslm@users.noreply.github.com>
2020-09-29 17:43:32 -0300

    Update next.tsx

--- src/screens/home/next.tsx
+++ src/screens/home/next.tsx
@@ -1,67 +1,14 @@
 import React, { useState } from "react";
 import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity } from "react-native";
-
-const DATA = [
- { time: '06:00', name: 'tempo livre', color:'#191919', colorText: '#ffffff' },
- { time: '06:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '07:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '07:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '08:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '08:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '09:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '09:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '10:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '10:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '11:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '11:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '12:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '12:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '13:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '13:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '14:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '14:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '15:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '15:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '16:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '16:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '17:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '17:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '18:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '18:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '19:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '19:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '20:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '20:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '21:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '21:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '22:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '22:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '23:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '23:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '00:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '00:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '01:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '01:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '02:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '02:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '03:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '03:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '03:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '03:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '03:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '03:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '03:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '04:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '04:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '05:00', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
- { time: '05:30', name: 'tempo livre', color:'#ffffff', colorText: '#191919' },
-];
+import { exempleData } from '../../exemples';
 
 const Item = ({ item, style, styleText }) => (
   <TouchableOpacity style={[styles.item, style]}>
     <Text style={[styles.title, styleText]}>{item.time}    {item.name} </Text>
   </TouchableOpacity>
 );
+
+const [data, setData] useState(exempleData);
 
 const lista = () => {
 
@@ -81,7 +28,7 @@ const lista = () => {
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
-        data={DATA}
+        data={data}
         renderItem={renderItem}
         //keyExtractor={(item) => item.id}
         //extraData={selectedId}
